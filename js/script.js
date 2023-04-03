4073b5da9f5ed0d20283e713d7923007ea818270
Fargun75 <113795596+Fargun75@users.noreply.github.com>
2023-04-03 18:38:25 +0200

    Log result after each round, and not just at the end

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
-        if (number == 0) {
+    if (number == 0) {
         return ("Rock");
-        
+
     } else if (number == 1) {
         return ("Paper");
 
@@ -13,16 +13,16 @@ function getComputerChoice () {
 
 function getUserChoice() {
     let playerInput = prompt("You choose - Rock, Paper or Scissors?: ");
-    let playerFirstletter = playerInput.slice(0,1);
+    let playerFirstletter = playerInput.slice(0, 1);
     let playerRestletters = playerInput.slice(1);
     let playerSelection = playerFirstletter.toUpperCase() + playerRestletters.toLowerCase();
-    return(playerSelection);
+    return (playerSelection);
 }
-    
-    
+
+
 
 function playRound(playerSelection, computerSelection) {
-    
+
     if (playerSelection === "Rock" && computerSelection === "Rock") {
         return ("It is a draw!");
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
@@ -34,16 +34,16 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         return ("You loose! Scissors beats Paper");
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return ("You loose! Rock beats Scissors"); 
+        return ("You loose! Rock beats Scissors");
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         return ("You win! Scissors beats Paper");
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return("It is a draw!");
+        return ("It is a draw!");
     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        return("It is a draw!");
-    
-    } 
-    
+        return ("It is a draw!");
+
+    }
+
 }
 
 let computerScore = 0;
@@ -59,35 +59,36 @@ function keepScore(playerSelection, computerSelection) {
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         return (++computerScore);
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return (++computerScore); 
+        return (++computerScore);
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         return (++playerScore);
-    } 
-    
+    }
+
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        
+
         let computerSelection = getComputerChoice();
         let playerSelection = getUserChoice();
-        
-        
+
+
         console.log("The computer selected: " + computerSelection);
         console.log("You selected: " + playerSelection);
         console.log(playRound(playerSelection, computerSelection));
         console.log("\n");
         keepScore(playerSelection, computerSelection);
-                
-        
+
+        console.log("Total score player: " + playerScore);
+        console.log("Total score computer: " + computerScore);
+        console.log("\n");
     }
-    console.log("Total score player: " + playerScore);
-    console.log("Total score computer: " + computerScore);
+
 }
 
 game();
 
 
 
- 
+
 
